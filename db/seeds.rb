03f767ef706b3bfeac20fb8661b369fb09157f66
Miles Higbie <miles.higbie@gmail.com>
2020-01-21 15:48:53 -0500

    added seeds.rb with the team populator that populated the DB

---
+++ db/seeds.rb
@@ -0,0 +1,24 @@
+require 'unirest'
+require "pry"
+
+# Teams API
+
+teams_api = Unirest.get "https://free-nba.p.rapidapi.com/teams",
+  headers:{
+    "X-RapidAPI-Host" => "free-nba.p.rapidapi.com",
+    "X-RapidAPI-Key" => "da38e79dd4msh1ce84dac4969474p12469djsnd69e872f4266"
+  }
+
+
+teams = {}
+teams = teams_api.body
+teams = teams["data"]
+
+def team_popluator(teams) 
+    teams.each do |team|
+        Team.create(:abbreviation => team["abbreviation"],
+        :city => team["city"], :conference => team["conference"], :division => team["division"], :full_name => team["full_name"], :name => team["name"]) 
+    end  
+end
+
+team_popluator(teams)
