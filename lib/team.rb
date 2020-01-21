6b90d6cb65e105bf9c6e034275229567aaa5d52f
Miles Higbie <miles.higbie@gmail.com>
2020-01-21 17:49:27 -0500

    added players method so Teams can return an array of their players

--- lib/team.rb
+++ lib/team.rb
@@ -1,3 +1,13 @@
 class Team < ActiveRecord::Base
 
+    def players
+    team_arr = []
+        Player.all.each do |player|
+            if self.id == player.team_id
+                team_arr.push(player)
+            end 
+        end 
+        team_arr
+    end
+
 end 
