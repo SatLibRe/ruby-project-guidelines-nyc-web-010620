70c411e4a881ceba3a798dd2c651b2aaecd641df
Miles Higbie <miles.higbie@gmail.com>
2020-01-21 21:13:47 -0500

    Verified the M2M relationship between game and players. Set the methods of up accordingly

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,25 @@
 class Game < ActiveRecord::Base
-    
+    has_many :teams 
+    has_many :players, through: :teams
+
+    def teams
+        Team.all.select do |team|
+            self.home_team_id == team.id || self.visitor_team_id == team.id
+        end 
+    end 
+
+    #looks like it works, need to find game that has players on home and away side 6 and 12?? Game 8
+    #verified
+    def players 
+     players_arr = [] 
+        teams.each do |team|
+           Player.all.each do |player|
+                if player.team_id == team.id 
+                    players_arr.push(player) 
+                end 
+           end  
+        end 
+        players_arr
+    end 
+
 end 
--- lib/team.rb
+++ lib/team.rb
@@ -1,4 +1,10 @@
 class Team < ActiveRecord::Base
     has_many :players
+    has_many :games
 
+    def games
+        Game.all.select do |game|
+            game.home_team_id == self.id || game.visitor_team_id == self.id
+        end 
+    end 
 end 
