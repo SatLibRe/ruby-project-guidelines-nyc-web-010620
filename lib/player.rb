ce16e779ec9ac6d09156cbc6ef5317ca72c2339e
Miles Higbie <miles.higbie@gmail.com>
2020-01-21 21:42:19 -0500

    Created the Many to Many realtionship for Player class. It did not work both ways before.:

--- lib/player.rb
+++ lib/player.rb
@@ -1,4 +1,10 @@
 class Player < ActiveRecord::Base
     belongs_to :team
     has_many :games, through: :team
+
+    def games 
+        Game.all.select do |game|
+            game.home_team_id == self.team_id || game.visitor_team_id == self.team_id
+        end 
+    end 
 end 
